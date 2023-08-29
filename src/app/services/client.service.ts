900c97decaa2aaad5c39d9a6aca2a120f564e2e2
Associate <Associate@roifmrwindt>
2023-08-29 13:43:41 +0530

    added models

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Client } from '../models/client';
+import { Person } from '../models/person';
+import { ClientIdentification } from '../models/clientIdentification';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
-
+  public data = [
+    
+  ]
+  public mockClientData = [
+    {
+      person: {
+        email: "",
+        id: 0,
+        dateOfBirth: new Date(),
+        country: '',
+        postalCode: '',
+      },
+      clientIdentificationSet : new Set<ClientIdentification>()
+    }
+  ];
   constructor() { }
 }
