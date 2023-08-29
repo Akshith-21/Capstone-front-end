5530c3147842dbb075d7ad76616b6b9c96a7398a
Associate <Associate@roifmrwindt>
2023-08-29 17:17:26 +0530

    integrated signup and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/signup/login/login.component';
 import { SigninComponent } from './components/signup/signin/signin.component';
-import { FormsModule } from '@angular/forms';
 import { NavComponent } from './components/nav/nav.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -10,8 +10,8 @@ import { ClientIdentification } from '../models/clientIdentification';
 
 export class ClientService {
   public mockClientData = new Map([
-    ["test@test.com", new Client(new Person('tests@test.com', 1, new Date('2001-01-01'), 'India', 'test'), new Set<ClientIdentification>([new ClientIdentification('test', 'test')]))],
-    ["mehulrana@gmail.com", new Client(new Person('mehulrana@gmail.com', 2, new Date("2001-04-05"), 'India', '411006'), new Set<ClientIdentification>([new ClientIdentification('PAN', '12345')]))],
+    ["test@test.com", new Client(new Person('tests@test.com', "1", String(new Date('2001-01-01')), 'India', 'test'), new Set<ClientIdentification>([new ClientIdentification('test', 'test')]))],
+    ["mehulrana@gmail.com", new Client(new Person('mehulrana@gmail.com', "2", String(new Date("2001-04-05")), 'India', '411006'), new Set<ClientIdentification>([new ClientIdentification('PAN', '12345')]))],
   ]);
   constructor() { }
 
